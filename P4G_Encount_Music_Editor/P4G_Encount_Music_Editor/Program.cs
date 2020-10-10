62b1c5bcd4234d5f3e0ddbe889f347aa654af826
T-Pose Ratkechi <tposeratkechi@gmail.com>
2020-10-10 02:09:16 -0500

    initial commit

---
+++ P4G_Encount_Music_Editor/P4G_Encount_Music_Editor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P4G_Encount_Music_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
