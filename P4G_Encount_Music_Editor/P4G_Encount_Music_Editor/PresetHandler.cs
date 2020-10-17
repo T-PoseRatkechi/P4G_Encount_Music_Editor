c8679fb0abf324913bb12adbe4ec720899c64c64
T-Pose Ratkechi <tposeratkechi@gmail.com>
2020-10-17 18:03:08 -0500

    working preset handler for setting encounter song indexes, collections or directly

---
+++ P4G_Encount_Music_Editor/P4G_Encount_Music_Editor/PresetHandler.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using static P4G_Encount_Music_Editor.Program;
+
+namespace P4G_Encount_Music_Editor
+{
+    class PresetHandler
+    {
+        private string currentDir = null;
+        private string presetsFolderDir = null;
+
+        public PresetHandler()
+        {
+            currentDir = Directory.GetCurrentDirectory();
+            presetsFolderDir = $@"{currentDir}\presets";
+        }
+
+        public void RunPreset(Encounter[] encounters)
+        {
+            // get preset file path
+            string presetFile = SelectPresetFile();
+            if (presetFile == null)
+                return;
+
+            try
+            {
+                string[] presetLines = File.ReadAllLines(presetFile);
+
+                foreach (string line in presetLines)
+                {
+                    // skip comment lines
+                    if (line.StartsWith("//") || line.Length <= 3)
+                        continue;
+
+                    string[] lineArgs = line.Split("=");
+
+                    // line is a command
+                    if (lineArgs[0].StartsWith('.'))
+                    {
+                        string collection = lineArgs[0].Substring(1);
+                        string command = lineArgs[1];
+                        RunCollectionCommand(encounters, collection, lineArgs[1]);
+                    }
+                    else
+                    {
+                        int encounterIndex = Int32.Parse(lineArgs[0]);
+                        ushort songIndex = UInt16.Parse(lineArgs[1]);
+
+                        Console.WriteLine($"Encounter Index: {encounterIndex} Song Index: {songIndex}");
+                        encounters[encounterIndex].MusicId = songIndex;
+                    }
+                    /*
+                    int encounterIndex = Int32.Parse(line.Replace(' ', '\0').Split("=")[0]);
+                    ushort songIndex = UInt16.Parse(line.Replace(' ', '\0').Split("=")[1]);
+
+                    Console.WriteLine($"Encounter Index: {encounterIndex} Song Index: {songIndex}");
+                    encounters[encounterIndex].MusicId = songIndex;
+                    */
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Problem reading preset file!");
+            }
+
+            Console.WriteLine("Encounter preset set. Enter to return to menu...");
+            Console.ReadLine();
+        }
+
+        private void RunCollectionCommand(Encounter[] encounters, string collectionName, string command)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Collection {collectionName.ToUpper()}:");
+            Console.ResetColor();
+
+            // store new music id to set to encounters
+            ushort newMusicId = 0;
+
+            // random set
+            if (command.StartsWith('.'))
+            {
+                if (command.StartsWith(".random"))
+                {
+                    try
+                    {
+                        ushort randSetIndex = UInt16.Parse(command.Split('-')[1]);
+                        newMusicId = (ushort)(randSetIndex + 8192);
+                        Console.WriteLine($"Using Random Set: {randSetIndex}, Song Index: {newMusicId}");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        Console.WriteLine("Problem parsing random set Song Index!");
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    newMusicId = UInt16.Parse(command);
+                    Console.WriteLine($"Using Song Index: {newMusicId}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine("Problem parsing Song Index!");
+                }
+            }
+
+            switch (collectionName)
+            {
+                case "all":
+                    Console.WriteLine($"Setting All Encounter Music IDs to: {newMusicId}");
+                    for (int i = 0, total = encounters.Length; i < total; i++)
+                    {
+                        encounters[i].MusicId = newMusicId;
+                    }
+                    break;
+                default:
+                    try
+                    {
+                        string collectionFilePath = $@"{currentDir}\collections\{collectionName}.enc";
+                        // exit if collection file is mising
+                        if (!File.Exists(collectionFilePath))
+                        {
+                            Console.WriteLine($"Collection file doesn't exist! File: {collectionFilePath}");
+                            return;
+                        }
+
+                        string[] collectionLines = File.ReadAllLines(collectionFilePath);
+
+                        foreach (string line in collectionLines)
+                        {
+                            // skip comment lines and new lines
+                            if (line.StartsWith('/') || line.StartsWith('\n') || line.Length < 1)
+                                continue;
+                            
+                            try
+                            {
+                                int encounterIndex = Int32.Parse(line);
+                                Console.WriteLine($"Encounter Index: {encounterIndex} Song Index: {newMusicId}!");
+                                encounters[encounterIndex].MusicId = newMusicId;
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                                Console.WriteLine($"Could not parse Encounter Index in collection: {collectionName}");
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        Console.WriteLine("Problem parsing collection!");
+                    }
+                    break;
+            }
+        }
+
+        private string SelectPresetFile()
+        {
+            string[] allPresets = null;
+
+            try
+            {
+                allPresets = Directory.GetFiles(presetsFolderDir, "*.preset", SearchOption.AllDirectories);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Problem reading presets from folder!");
+                return null;
+            }
+
+            // exit if no presets were found
+            if (allPresets.Length <= 0)
+                return null;
+
+            // display all presets
+            Console.WriteLine("Available Presets");
+            for (int i = 0, total = allPresets.Length; i < total; i++)
+            {
+                Console.WriteLine($"{i}. {Path.GetFileNameWithoutExtension(allPresets[i])}");
+            }
+
+            // get valid preset choice
+            int presetSelection = -1;
+            do
+            {
+                int tempChoice = PromptInt("Preset Selection");
+                if (tempChoice >= allPresets.Length)
+                    Console.WriteLine("Invalid selection!");
+                else
+                    presetSelection = tempChoice;
+
+            } while (presetSelection < 0);
+
+            return allPresets[presetSelection];
+        }
+
+        private static int PromptInt(string name)
+        {
+            int theNumber = -1;
+
+            while (theNumber < 0)
+            {
+                Console.Write($"Enter {name} (number): ");
+                string input = Console.ReadLine();
+                try
+                {
+                    theNumber = Int32.Parse(input);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Couldn't parse numer!");
+                }
+            }
+
+            return theNumber;
+        }
+    }
+}
--- P4G_Encount_Music_Editor/P4G_Encount_Music_Editor/Program.cs
+++ P4G_Encount_Music_Editor/P4G_Encount_Music_Editor/Program.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Buffers.Binary;
 using static P4G_Encount_Music_Editor.Enums;
+using System.Text;
+using System.Linq;
 
 namespace P4G_Encount_Music_Editor
 {
     class Program
     {
-        private struct Encounter
+        public struct Encounter
         {
             public byte[] Flags { get; set; }
             public ushort Field04 { get; set; }
@@ -24,11 +26,98 @@ namespace P4G_Encount_Music_Editor
         private static string moddedFolderDir = null;
         private static string presetsFolderDir = null;
 
+        private static PresetHandler presetHandler = new PresetHandler();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ParseArkFile();
             SetPaths();
             EditEncount();
+        }
+
+        private static void ParseArkFile()
+        {
+            string arkFile = $@"{Directory.GetCurrentDirectory()}\Encounter_Place_ID.txt";
+            string inEncountPath = $@"{Directory.GetCurrentDirectory()}\original\ENCOUNT.TBL";
+
+            Encounter[] allEncounters = GetEncountersList(inEncountPath);
+
+            string[] arkLines = File.ReadAllLines(arkFile);
+
+            Dictionary<string, List<int>> encountersList = new Dictionary<string, List<int>>();
+
+            foreach(string line in arkLines)
+            {
+                if (!line.Contains('-') || line.Length < 3 || line.StartsWith("//"))
+                    continue;
+
+                int encounterIndex = Int32.Parse(line.Split(" - ")[0].Split(' ')[2]);
+                // get full encounter location/description
+                string encounterLocation = line.Split(" - ")[1];
+                // remove question marks
+                //encounterLocation = encounterLocation.Replace('?', '\0');
+                if (encounterLocation.StartsWith('?'))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Encounter {encounterIndex} is unknown!");
+                    //Console.ReadLine();
+                    Console.ResetColor();
+                    continue;
+                }
+
+                encounterLocation = encounterLocation.Replace('?', '\0');
+
+                string locationName = $"{encounterLocation.Split(' ')[0]}{(encounterLocation.Split(' ')[0].ToLower().Equals("heaven") ? "" : $" {encounterLocation.Split(' ')[1]}")}";
+                locationName = locationName.Trim('\0');
+
+                Console.WriteLine($"Encounter Index: {encounterIndex} Location: {locationName}");
+                if (encountersList.ContainsKey(locationName))
+                {
+                    encountersList[locationName].Add(encounterIndex);
+                }
+                else
+                {
+                    encountersList.Add(locationName, new List<int>());
+                    encountersList[locationName].Add(encounterIndex);
+                }
+            }
+
+            StringBuilder parsedFile = new StringBuilder();
+
+            foreach (string locationKey in encountersList.Keys)
+            {
+                string collectionFile = $@"{Directory.GetCurrentDirectory()}\collections\{locationKey}.enc";
+                StringBuilder collectionText = new StringBuilder();
+
+                parsedFile.AppendLine($"{locationKey.ToUpper()}\n#================================================");
+                collectionText.AppendLine($"//{locationKey.ToUpper()}\n//================================================");
+
+                foreach (int encountId in encountersList[locationKey])
+                {
+                    Encounter currentEncounter = allEncounters[encountId];
+                    parsedFile.AppendLine($"Encounter Index: {encountId}");
+                    collectionText.AppendLine($"//Encounter Index:\n{encountId}");
+                    
+                    parsedFile.Append("#Encounter Units: ");
+                    collectionText.Append("//Encounter Units: ");
+
+                    foreach (EnemiesID enemy in currentEncounter.Units)
+                    {
+                        if (enemy != EnemiesID.h000)
+                        {
+                            parsedFile.Append($"{enemy.ToString()}, ");
+                            collectionText.Append($"{enemy.ToString()}, ");
+                        }
+                    }
+                    parsedFile.Append('\n');
+                    collectionText.Append("\n\n");
+                }
+                parsedFile.Append('\n');
+                //collectionText.Append('\n');
+                File.WriteAllText(collectionFile, collectionText.ToString());
+            }
+
+            File.WriteAllText($"{arkFile}.parsed", parsedFile.ToString());
         }
 
         private static void SetPaths()
@@ -43,6 +132,7 @@ namespace P4G_Encount_Music_Editor
             {
                 Directory.CreateDirectory(originalFolderDir);
                 Directory.CreateDirectory(moddedFolderDir);
+                Directory.CreateDirectory(presetsFolderDir);
             }
             catch (Exception e)
             {
@@ -64,7 +154,34 @@ namespace P4G_Encount_Music_Editor
 
             Encounter[] allBattles = GetEncountersList(inEncountPath);
 
-            RandomizeEncounterBgm(allBattles);
+            int menuSelection = 0;
+
+            do
+            {
+                Console.WriteLine("P4G_Encount_Music_Editor");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Run Preset");
+                Console.WriteLine("2. Build Config");
+                Console.WriteLine("3. Output Encounter List");
+                Console.WriteLine("0. Exit and Save");
+
+                menuSelection = PromptInt("Menu Selection");
+                switch (menuSelection)
+                {
+                    case 1:
+                        presetHandler.RunPreset(allBattles);
+                        break;
+                    case 2:
+                        presetHandler.RunPreset(allBattles);
+                        break;
+                    case 3:
+                        OutputEncounterList(allBattles);
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (menuSelection != 0);
 
             using (BinaryWriter writer = new BinaryWriter(File.Open(outEncountPath, FileMode.Create)))
             {
@@ -89,25 +206,29 @@ namespace P4G_Encount_Music_Editor
             }
         }
 
-        private static void RandomizeEncounterBgm(Encounter[] encounters)
+        private static void OutputEncounterList(Encounter[] encounters)
         {
-            Console.Clear();
-            Console.WriteLine($"Randomizing Encounter MusicIds");
-            Console.WriteLine("Input index range. Indexes will equal Min to Max - 1. Enter 0 and 0 for BGME default range (886-895).");
-
-            int min = PromptInt("Min");
-            int max = PromptInt("Max");
-
-            if (min == 0 && max == 0)
+            try
             {
-                min = 886;
-                max = 895 + 1;
+                string listFilePath = $@"{currentDir}\encounters.txt";
+
+                StringBuilder listText = new StringBuilder();
+
+                for (int i = 0, total = encounters.Length; i < total; i++)
+                {
+                    listText.AppendLine($"Encounter Index: {i} Song Index: {encounters[i].MusicId}");
+                    foreach(EnemiesID enemy in encounters[i].Units)
+                    {
+                        listText.AppendLine(enemy.ToString());
+                    }
+                }
+
+                File.WriteAllText(listFilePath, listText.ToString());
             }
-
-            Random rand = new Random();
-            for (int i = 0, total = encounters.Length; i < total; i++)
+            catch (Exception e)
             {
-                encounters[i].MusicId = (ushort)(rand.Next(min, max));
+                Console.WriteLine(e);
+                Console.WriteLine("Problem writing encounter list!");
             }
         }
 
@@ -136,7 +257,7 @@ namespace P4G_Encount_Music_Editor
         {
             List<Encounter> allEncounters = new List<Encounter>();
 
-            Console.WriteLine("Parsing ENCOUNT.TBL...");
+            Console.WriteLine("Parsing ENCOUNT.TBL");
 
             try
             {
