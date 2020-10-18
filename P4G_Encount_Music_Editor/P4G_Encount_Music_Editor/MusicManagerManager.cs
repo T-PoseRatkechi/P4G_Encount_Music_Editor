62c76bcd71cb9f953aca748897f14f1f2ad50fc6
T-Pose Ratkechi <tposeratkechi@gmail.com>
2020-10-18 02:16:30 -0500

    added class to handle music manager related files

---
+++ P4G_Encount_Music_Editor/P4G_Encount_Music_Editor/MusicManagerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace P4G_Encount_Music_Editor
+{
+    public class idtowaveindex
+    {
+        public conversionObject[] conversions { get; set; }
+    }
+
+    public class conversionObject
+    {
+        public string trackID { get; set; }
+        public string waveIndex { get; set; }
+        public string uwusID { get; set; }
+    }
+
+    class MusicManagerManager
+    {
+        private static string currentDir = null;
+
+        public MusicManagerManager()
+        {
+            currentDir = Directory.GetCurrentDirectory();
+        }
+
+
+    }
+}
